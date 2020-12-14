a2a1574262c81221dbe99b45b5a7859eeae1875a
Carmelo Mammana <carmelomammana8@gmail.com>
2020-12-14 16:30:57 +0100

    plane script

---
+++ BalanceColour/Assets/Scripts/PlaneController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    public float rotSpeed;
+
+    private Rigidbody rb;
+    private float xRot;
+    private float zRot;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        xRot = Input.acceleration.x * rotSpeed;
+        zRot = Input.acceleration.y * rotSpeed;
+
+        transform.rotation = new Quaternion(xRot, 0, zRot, 0);
+    }
+}
