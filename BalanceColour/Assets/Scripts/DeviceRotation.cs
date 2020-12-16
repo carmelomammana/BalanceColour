d82483da86b6d0dc832398a299ed48e60b9563f8
Carmelo Mammana <carmelomammana8@gmail.com>
2020-12-16 16:50:24 +0100

    joystick & rotation

---
+++ BalanceColour/Assets/Scripts/DeviceRotation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DeviceRotation
+{
+    private static bool gyroInitialized = false;
+
+    public static bool HasGyroscope
+    {
+        get
+        {
+            return SystemInfo.supportsGyroscope;
+        }
+    }
+
+    public static Quaternion Get()
+    {
+        if (!gyroInitialized)
+            InitGyro();
+
+        return HasGyroscope ? ReadyGyroscopeRotation() : Quaternion.identity;
+    }
+
+    private static void InitGyro()
+    {
+        if (HasGyroscope)
+        {
+            Input.gyro.enabled = true;
+            Input.gyro.updateInterval = 0.0167f;
+        }
+        gyroInitialized = true;
+    }
+
+    private static Quaternion ReadyGyroscopeRotation()
+    {
+        Quaternion q = new Quaternion(1, 1, 0, 0) * Input.gyro.attitude;//* new Quaternion(0, 0, 0, 0);
+        return new Quaternion(q.x, q.z, -q.y, q.w);
+    }
+}
--- BalanceColour/Assets/Scripts/PlaneController.cs
+++ BalanceColour/Assets/Scripts/PlaneController.cs
@@ -5,21 +5,17 @@ using UnityEngine;
 public class PlaneController : MonoBehaviour
 {
     public float rotSpeed;
-
+    public FloatingJoystick joystick;
     private Rigidbody rb;
-    private float xRot;
-    private float zRot;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+       
     }
 
     void Update()
     {
-        xRot = Input.acceleration.x * rotSpeed;
-        zRot = Input.acceleration.y * rotSpeed;
-
-        transform.rotation = new Quaternion(xRot, 0, zRot, 0);
+        this.transform.eulerAngles = new Vector3((joystick.Vertical * 90) / Mathf.PI, 0, -(joystick.Horizontal * 90) / Mathf.PI);
     }
 }
