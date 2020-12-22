406f1d96843dba6c3faef9d988f8e099fd00941a
Carmelo Mammana <carmelomammana8@gmail.com>
2020-12-22 17:39:44 +0100

    tag & trigger

--- BalanceColour/Assets/Scripts/Ball.cs
+++ BalanceColour/Assets/Scripts/Ball.cs
@@ -15,4 +15,27 @@ public class Ball : MonoBehaviour
     {
         
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "Hole_blue":
+                if (colour.name.ToLower() == "blue")
+                    Debug.Log("blue");
+                break;
+            case "Hole_green":
+                if (colour.name.ToLower() == "green")
+                    Debug.Log("green");
+                break;
+            case "Hole_yellow":
+                if (colour.name.ToLower() == "yellow")
+                    Debug.Log("yellow");
+                break;
+            case "Hole_red":
+                if (colour.name.ToLower() == "red")
+                    Debug.Log("red");
+                break;
+        }
+    }
 }
